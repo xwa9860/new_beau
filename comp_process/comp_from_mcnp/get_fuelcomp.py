3464bcdb070b410a9d4d9f3ec758ecfb43a0b077
Xin Wang <imwangxin@gmail.com>
2015-05-08 11:18:01 -0700

    added gitignore to ignore runrtp files, ran with coolant at 600 and 700 celsius

--- comp_process/comp_from_mcnp/get_fuelcomp.py
+++ comp_process/comp_from_mcnp/get_fuelcomp.py
@@ -11,13 +11,15 @@ for p in range(1, 9):
 			with open('mk1.txt', 'r+') as f:
 				for line in f:
 					if mat_name in line:
+						print line
 						#mat_comp.append(line)
 						ne = f.next()
+						print ne
 						while 'm' not in ne:
 							mat_comp.append(ne.split()[0]+' %.8e' %(float(ne.split()[1])+float(ne.split()[3]) )+ '\n')
 							ne = f.next()
 							nb_isotope = nb_isotope + 1
-						print nb_isotope
+						#print nb_isotope
 				with open(mat_name, 'w+') as outputf:
 					outputf.write(''.join(mat_comp))
 				outputf.close()
